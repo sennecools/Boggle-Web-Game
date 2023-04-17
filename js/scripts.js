b07357845c44243ea6d4de8e69f6193d4b95bcb0
sennecools <113857183+sennecools@users.noreply.github.com>
2023-04-17 21:25:15 +0200

    Herwerking van zowat alle bestanden. -Senne Cools

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,56 @@
-// On load 
+/*
+*********************************************
+*                                           *
+*            On webpage load                *
+*                                           *
+*********************************************
+- aanmaken constanten
+- event listeners toevoegen
+- functie shuffleLetters
+*/
+
 const addButton = document.querySelector('#addWord');
+const boardButtons = document.querySelectorAll("#board button");
 const letterButtons = document.querySelectorAll('.letterButton');
+const resultaat = document.getElementById("resultaat");
 const wordDiv = document.querySelector("#word");
-document.querySelector("#shuffle").addEventListener("click", shuffleLetters);
+document.querySelector("#addWord").addEventListener("click", addWordButton);
 document.querySelector("#undo").addEventListener("click", undoButton);
 let addWord = 0;
 let wordPoints;
 
 shuffleLetters()
 
-const boardButtons = document.querySelectorAll("#board button");
 boardButtons.forEach(button => {
     button.addEventListener("click", () => {
-        buttonClick(button);
+        boardLetterButton(button);
     });
 });
 
-// Functions
+/*
+*********************************************
+*                                           *
+*            Functions                      *
+*                                           *
+*********************************************
+- shuffleLetters
+- boardLetterButton
+- undoButton
+- addWordButton
+- letterButtons
+- timer
+*/
 
+// Geeft elke button een random letter als er teveel medeklinkers zijn worden er klinkers bijgevoegd
 function shuffleLetters() {
     const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Qu", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     const vowels = ["A", "E", "I", "O", "U"];
-    const buttons = document.querySelectorAll("#board button");
 
+    let randomIndex, letter;
     let vowelCount = 0;
     let consonantCount = 0;
-    let randomIndex, letter;
 
-    // count the number of vowels and consonants in the array
+    // telt de klinkers en medeklinkers in de array
     letters.forEach(letter => {
         if (vowels.includes(letter)) {
             vowelCount++;
@@ -36,18 +59,18 @@ function shuffleLetters() {
         }
     });
 
-    // if there are not enough vowels, replace some consonants with vowels
+    // zolang de klinker teller minder dan 3 is worden er medeklinkers vervangen
     while (vowelCount < 3) {
         randomIndex = Math.floor(Math.random() * consonantCount);
         letter = letters.find(letter => !vowels.includes(letter));
-        buttons[randomIndex].textContent = letter;
+        boardButtons[randomIndex].textContent = letter;
         letters[randomIndex] = letter;
         consonantCount--;
         vowelCount++;
     }
 
-    // shuffle the remaining letters
-    buttons.forEach(button => {
+    // shuffled de letters
+    boardButtons.forEach(button => {
         randomIndex = Math.floor(Math.random() * letters.length);
         letter = letters[randomIndex];
         button.textContent = letter;
@@ -55,37 +78,65 @@ function shuffleLetters() {
     });
 }
 
-
-
-function buttonClick(button) {
-    const letter = button.textContent;
-    button.disabled = true;
-
+function boardLetterButton(button) {
     const currentWord = wordDiv.textContent;
-    wordDiv.textContent = currentWord + letter;
+    const letter = button.textContent;
     const shuffle = document.querySelector("#shuffle")
-    shuffle.disabled = true;
+    button.disabled = true;
+    wordDiv.textContent = currentWord + letter;
 }
 
+// Zet alle buttons terug aan, zet "woord" weer op NULL 
 function undoButton() {
-    const buttons = document.querySelectorAll(".letterButton");
-    buttons.forEach(button => {
-        button.disabled = false;
-    });
+    const addWordButton = document.querySelector("#addWord");
+    const pressedButtons = document.querySelectorAll('.letterButton');
     const shuffle = document.querySelector(".shuffle");
-    shuffle.disabled = false;
     const wordDiv = document.querySelector("#word");
-    wordDiv.textContent = null;
-    const pressedButtons = document.querySelectorAll('.letterButton.pressed');
 
-    pressedButtons.forEach(button => {
-        button.classList.remove('pressed');
-        button.classList.add('letterButton');
+    // Enabled alle buttons dat de class "pressed" heeft
+    pressedButtons.forEach(pressedButtons => {
+        pressedButtons.disabled = false;
+    });
+
+    // Verwijderd de class "pressed" van alle buttons
+    pressedButtons.forEach(pressedButtons => {
+        pressedButtons.classList.remove('pressed');
     });
     addWord = 0;
-    const addWordButton = document.querySelector("#addWord");
     addWordButton.disabled = true;
+    wordDiv.textContent = null;
+}
 
+// Stuurt een verzoek naar de API om te checken of het woord wel bestaat (momenteel alleen engels)
+async function addWordButton() {
+    let word = wordDiv.textContent;
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    const resp = await fetch(url);
+    const data = await resp.json();
+    console.log(data);
+
+    if (data.title == "No Definitions Found") {
+        resultaat.innerText = `${word}: woord bestaat niet`;
+        undoButton();
+    }
+    else {
+        if (word.length == 3 || word.length == 4) {
+            resultaat.innerText = `${word}: is 1 punt`
+        }
+        else if (word.length == 5) {
+            resultaat.innerText = `${word}: is 2 punten`
+        }
+        else if (word.length == 6) {
+            resultaat.innerText = `${word}: is 3 punten`
+        }
+        else if (word.length == 7) {
+            resultaat.innerText = `${word}: is 5 punten`
+        }
+        else if (word.length >= 8) {
+            resultaat.innerText = `${word}: is 11 punten`
+        }
+        undoButton();
+    }
 }
 
 letterButtons.forEach(button => {
@@ -97,21 +148,21 @@ letterButtons.forEach(button => {
         const isRightColumn = square % 4 === 0;
         button.classList.add("pressed");
         addWord++;
-        console.log(addWord);
 
+        // Zolang "addWord" niet groter is dan 2 is de addWord button disabled
         if (addWord >= 3) {
             const addWord = document.querySelector("#addWord");
             addWord.disabled = false;
         }
 
-        // Disable all other buttons
+        // Disabled alle andere buttons
         letterButtons.forEach(otherButton => {
             if (otherButton !== button) {
                 otherButton.disabled = true;
             }
         });
 
-        // Enable adjacent buttons
+        // Enabled alle aanliggende buttons
         if (!isTopRow && !isLeftColumn) {
             const topLeftButton = document.querySelector(`[data-square="${square - 5}"]`);
             topLeftButton.disabled = false;
@@ -151,6 +202,8 @@ letterButtons.forEach(button => {
             const bottomRightButton = document.querySelector(`[data-square="${square + 5}"]`);
             bottomRightButton.disabled = false;
         }
+
+        // Zorgt ervoor dat een button geen twee keer kan gedrukt worden
         const pressedButtons = document.querySelectorAll(".pressed");
         pressedButtons.forEach(button => {
             button.disabled = true;
@@ -158,17 +211,20 @@ letterButtons.forEach(button => {
     });
 });
 
-//timer
+// Timer van 3 minuten als de timer gedaan is worden alle buttons gedisabled
 const duration = 180;
 const timerEl = document.querySelector('#timer');
 let timeLeft = duration;
 
-// Create an interval that updates the timer every second
+// Zorgt dat de tekst elke seconde wordt geupdate
 const countdownInterval = setInterval(() => {
     timeLeft--;
-    timerEl.innerText = timeLeft;
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+    const formattedTime = `Time ${minutes}:${seconds.toString().padStart(2, '0')}`;
+    timerEl.innerText = formattedTime;
 
-    // Check if the timer has reached zero
+    // Checken of de timer nog niet 0 is
     if (timeLeft === 0) {
         clearInterval(countdownInterval);
         letterButtons.forEach(button => {
@@ -177,36 +233,3 @@ const countdownInterval = setInterval(() => {
         timerEl.innerText = 'Time\'s up!';
     }
 }, 1000);
-
-addWordButton();
-async function addWordButton() {
-    let word;
-    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-    const resp = await fetch(url);
-    const data = await resp.json();
-    console.log(data.title);
-    addButton.addEventListener(`click`, function () {
-        word = wordDiv.textContent;
-        if (data.title == "No Definitions Found") {
-            resultaat.innerText = `${word}: woord bestaat niet`;
-        }
-        else {
-            if (word.length == 3 || word.length == 4) {
-                resultaat.innerText = `${word}: is 1 punt`
-            }
-            else if (word.length == 5) {
-                resultaat.innerText = `${word}: is 2 punten`
-            }
-            else if (word.length == 6) {
-                resultaat.innerText = `${word}: is 3 punten`
-            }
-            else if (word.length == 7) {
-                resultaat.innerText = `${word}: is 5 punten`
-            }
-            else if (word.length >= 8) {
-                resultaat.innerText = `${word}: is 11 punten`
-            }
-            undoButton()
-        }
-    });
-}
